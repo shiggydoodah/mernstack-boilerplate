0ac1e797642f1cefbefbc7ad611a719591881fb5
shiggydoodah <louis@louislombardi.co.uk>
2020-04-11 14:24:44 +0100

    prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb://root:root@localhost:27017/mernstack'
+    // 'mongodb://root:root@localhost:27017/mernstack'
+    'mongodb://lldesign:K5TZa7^^VoUx%UOGV0YriP@localhost:27017/mernstack'
 }
